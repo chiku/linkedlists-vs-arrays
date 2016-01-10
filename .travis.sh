de75cb49d734f7a4865f722b62cac7877a8936cb
Chirantan Mitra <chirantan.mitra@gmail.com>
2016-01-10 12:07:10 +0530

    Run golang on travis

---
+++ .travis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+golang_prereqs() {
+  sudo apt-get update
+  sudo apt-get instal golang
+  go get github.com/mattn/gom
+}
+}
+
+golang_build() {
+  cd arraylist && gom test -coverprofile=coverage.out && gom tool cover -html=coverage.out -o coverage.html && cd ..
+  cd linkedlist && gom test -coverprofile=coverage.out && gom tool cover -html=coverage.out -o coverage.html && cd ..
+  gom install github.com/chiku/linkedlists-vs-arrays/compare
+  gom install github.com/chiku/linkedlists-vs-arrays/create_report
+}
+
+main() {
+  golang_prereqs
+  golang_build
+}
+
+main
