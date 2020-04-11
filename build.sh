c1b0b30961b6d37b4a63d3e7ad891b5dd660d875
Chirantan Mitra <chirantan.mitra@gmail.com>
2020-04-11 11:25:08 +0530

    Generate HTML coverage

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 main() {
-  go test -cover ./...
+  go test -coverprofile=coverage.out ./...
+  go tool cover -html=coverage.out -o coverage.html
   go build -o ./bin/compare ./cmd/compare
   go build -o ./bin/create-report ./cmd/createreport
 }
